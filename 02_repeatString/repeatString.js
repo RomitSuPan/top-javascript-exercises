a57c19833c3c0c7d0a373b39a8502592da0a2c36
RomitSuPan <129577622+RomitSuPan@users.noreply.github.com>
2024-03-07 21:05:00 -0700

    "Hellow World, Repeat String Done"

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, number_of_repetitions) {
+    if (number_of_repetitions >= 1) {
+        for(i = 0; i < number_of_repetitions ; i++) {
+            return string.repeat(number_of_repetitions)
+        }
+    }
+    else if(number_of_repetitions == 0){
+        return ""
+    }
+    else {
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
