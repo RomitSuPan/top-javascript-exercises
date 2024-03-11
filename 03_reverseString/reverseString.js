b81f5ea404c888cfbb88a5e39c9d31d90003ba3b
RomitSuPan <129577622+RomitSuPan@users.noreply.github.com>
2024-03-10 23:16:50 -0600

    reverse String, remove from Array done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(original_string) {
+    split_string = original_string.split("");
+    return split_string.reverse().join("");
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(...array_and_values_to_remove) {
+    original_array = array_and_values_to_remove[0];
+    values_to_remove = array_and_values_to_remove.slice(1);
+    console.log(original_array);
+    console.log(values_to_remove);
+   
+    filtered_array = []
 
+    for (let i = 0; i < original_array.length ; i++) {
+        if (!values_to_remove.includes(original_array[i])) {
+            filtered_array.push(original_array[i]);
+        }
+    }
+
+    return filtered_array;
 };
 
 // Do not edit below this line
