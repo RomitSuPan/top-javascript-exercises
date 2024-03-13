9955b282437c9df21b100fa8cbb6cd8d0b1cc6bb
RomitSuPan <129577622+RomitSuPan@users.noreply.github.com>
2024-03-12 22:47:16 -0600

    05_sumAll done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const { lowerCase } = require("case-anything");
 
+const sumAll = function(val_1, val_2) {
+    if ((val_1 >= 0 && Number.isInteger(val_1)) && (val_2 >= 0 && Number.isInteger(val_2))) {
+        if (val_1 <= val_2) {
+            lower_limit = val_1;
+            upper_limit = val_2;
+        } else {
+            lower_limit = val_2;
+            upper_limit = val_1; 
+        }
+        sum = 0;
+        for (let i = lower_limit; i <= upper_limit; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+    else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
