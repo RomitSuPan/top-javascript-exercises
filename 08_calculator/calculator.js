9b2fa901723b6d6d2a8c9bce461a15bdd45e4b30
RomitSuPan <129577622+RomitSuPan@users.noreply.github.com>
2024-03-15 15:23:01 -0600

    08_calculator done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return ((num1) + (num2));
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return ((num1) - (num2));
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+	let sum = 0;
+  numArray.forEach(element => {
+    sum += element;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+	product = 1;
+  numArray.forEach(element => {
+    product = product * element;
+  });
+  return product;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return ((num1) ** (num2));
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+  let result = 1;
+  for (let i = 1; i <= num1 ; i++) {
+    result = result * i;
+  }
+  return result;
 };
 
 // Do not edit below this line
