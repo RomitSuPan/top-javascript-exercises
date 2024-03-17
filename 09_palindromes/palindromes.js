c55da7416b22ac7862d7c5b965100168ddcd750c
RomitSuPan <129577622+RomitSuPan@users.noreply.github.com>
2024-03-16 22:41:22 -0600

    Solve 09 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (original_str) {
+    original_str = original_str.replace(/[^a-zA-Z0-9]/g, '');
+    original_array = original_str.toLowerCase().replaceAll(' ', '').split("");
+    original_str = original_array.toString();
+    reversed_str = original_array.toReversed().toString();
+    if (original_str === reversed_str) {
+        return true;
+    } else {
+        return false;
+    }
 };
-
+palindromes('Racecar!')
 // Do not edit below this line
 module.exports = palindromes;
