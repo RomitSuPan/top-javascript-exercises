ed71fde5ef655b2690f01b088d80637bd259c9e2
RomitSuPan <129577622+RomitSuPan@users.noreply.github.com>
2024-03-17 11:57:25 -0600

    Fibonacci solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,30 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    num1 = 1;
+    num2 = 1;
+    sum = 0;
+    let i = 3;
+    if (number < 0) {
+        return "OOPS";
+    }
+    else if (number == 0) {
+        return 0;
+    }
+    else if (number < 3) {
+        return 1;
+    }
+    else if (number >= 3) {
+        while (i <= number) {
+            sum = num1 + num2;
+            num1 = num2;
+            num2 = sum;
+            i++;
+            console.log(sum);
+        }
+        return sum;
+    }
 };
+
+// fibonacci(4);
 
 // Do not edit below this line
 module.exports = fibonacci;
