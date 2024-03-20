ffe718aa6c700250c931b352401a5116534968af
RomitSuPan <129577622+RomitSuPan@users.noreply.github.com>
2024-03-19 22:47:25 -0600

    Find the oldes done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(persons) {
+    let highest_age = 0;
+    let longest_life_person;
+    const d = new Date();
+    const year = d.getFullYear();
+    persons.forEach(element => {
+        if ('yearOfDeath' in element) {
+            age = element.yearOfDeath - element.yearOfBirth;
+        } else {
+            age = year - element.yearOfBirth;
+        }
+        if (age > highest_age) {
+            longest_life_person = element;
+            highest_age = age;
+        }
+    });
+    return longest_life_person;
 };
 
 // Do not edit below this line
